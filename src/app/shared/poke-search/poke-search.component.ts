50b73bb6f1563addc5e5232c8e0ceed95fca92dd
plcrisp <pedrolcrisp@gmail.com>
2023-10-27 17:38:34 -0300

    Pokedex base

---
+++ src/app/shared/poke-search/poke-search.component.ts
@@ -0,0 +1,15 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+
+@Component({
+  selector: 'poke-search',
+  templateUrl: './poke-search.component.html',
+  styleUrls: ['./poke-search.component.scss']
+})
+export class PokeSearchComponent {
+  @Output() public emitSearch: EventEmitter<String> = new EventEmitter();
+
+  public search(value: string){
+    this.emitSearch.emit(value);
+  }
+
+}
