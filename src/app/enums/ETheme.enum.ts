21a294aea26db5f329339bec259d566c32fcd33f
plcrisp <pedrolcrisp@gmail.com>
2023-10-30 20:03:49 -0300

    dark theme

---
+++ src/app/enums/ETheme.enum.ts
@@ -0,0 +1,6 @@
+export enum ETheme{
+    ICON_MOON = 'moon',
+    ICON_SUN = 'sun',
+    TEXT_SUN = 'Light theme',
+    TEXT_MOON = 'Dark theme',
+}
--- src/app/shared/poke-header/poke-header.component.ts
+++ src/app/shared/poke-header/poke-header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ETheme } from 'src/app/enums/ETheme.enum';
 
 @Component({
   selector: 'poke-header',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./poke-header.component.scss']
 })
 export class PokeHeaderComponent {
+  public icon: string = ETheme.ICON_MOON;
+  public textTheme: string = ETheme.TEXT_MOON;
 
+  public toggle(){
+    const theme = document.body.classList.toggle('dark-theme');
+
+    if(theme){
+      this.textTheme = ETheme.TEXT_SUN;
+      return (this.icon = ETheme.ICON_SUN);
+    }
+    this.textTheme = ETheme.TEXT_MOON;
+    return (this.icon = ETheme.ICON_MOON);
+  }
 }
